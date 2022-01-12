dcba84fa5013ea3d355a3f48615bbde6116e27d6
Daskl01 <aboubackrskl@gmail.com>
2022-01-12 15:42:00 +0000

    End Projects

--- Back-end/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-require("dotenv").config({path: "./config/.env"});
-require("./config/db.js");
-const app = express();
-
-app.listen(process.env.PORT, () => {
-    console.log(`server starting on port ${process.env.PORT}`);
-})
-
